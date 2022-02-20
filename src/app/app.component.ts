1d8bf9b812b39092c637ad08ef82fc9f8065c24a
malak <malaknakaa@gmail.com>
2022-02-20 20:57:10 +0100

    charts apex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,173 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexLegend,
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexPlotOptions
+} from "ng-apexcharts";
+import { series } from "./data";
+
+
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  yaxis: ApexYAxis;
+  title: ApexTitleSubtitle;
+  labels: string[];
+  legend: ApexLegend;
+  subtitle: ApexTitleSubtitle;
+};
+
+export type SecondchartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
+export type ThirdChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  plotOptions: ApexPlotOptions;
+};
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'apexchart';
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions> | any;
+  public secondchartOptions: Partial<SecondchartOptions> | any;
+  public thirdchartOptions: Partial<ThirdChartOptions> | any;
+
+
+
+	isEnabled: false;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "STOCK ABC",
+          data: series.monthDataSeries1.prices
+        }
+      ],
+      chart: {
+        type: "area",
+        height: 350,
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+
+      title: {
+        text: "Fundamental Analysis of Stocks",
+        align: "left"
+      },
+      subtitle: {
+        text: "Price Movements",
+        align: "left"
+      },
+      labels: series.monthDataSeries1.dates,
+      xaxis: {
+        type: "datetime"
+      },
+      yaxis: {
+        opposite: true
+      },
+      legend: {
+        horizontalAlign: "left"
+      }
+    };
+    this.secondchartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    this.thirdchartOptions = {
+      series: [
+        {
+          data: [
+            {
+              x: "Code",
+              y: [
+                new Date("2019-03-02").getTime(),
+                new Date("2019-03-04").getTime()
+              ]
+            },
+            {
+              x: "Test",
+              y: [
+                new Date("2019-03-04").getTime(),
+                new Date("2019-03-08").getTime()
+              ]
+            },
+            {
+              x: "Validation",
+              y: [
+                new Date("2019-03-08").getTime(),
+                new Date("2019-03-12").getTime()
+              ]
+            },
+            {
+              x: "Deployment",
+              y: [
+                new Date("2019-03-12").getTime(),
+                new Date("2019-03-18").getTime()
+              ]
+            }
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "rangeBar"
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgApexchartsModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
